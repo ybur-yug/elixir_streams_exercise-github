cbf2a3baeadcf80797e3a80c19c5dd2f62bb1ba1
Ybur Yug <bobbygrayson@gmail.com>
2016-02-06 16:17:10 -0400

    add doc

--- lib/github_api.ex
+++ lib/github_api.ex
@@ -1,6 +1,7 @@
 defmodule GithubAPI do
   alias GithubAPI.ResultStream
 
+  @doc "GET#index Repositories for a given org"
   @spec repos(String.t) :: GithubAPI.ResultStream.t
   def repos(organization), do: ResultStream.new("/orgs/#{organization}/repos")
 end
